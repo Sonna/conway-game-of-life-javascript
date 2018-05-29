d93344396ce9ae42c059892b4371f08ea0ebfc00
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-05-29 13:26:26 +1000

    Update & Remove package dependencies
    Remove the unused `nock` dependency and update remaining package
    dependencies for this application to remove possible vulnerabilities,
    whilst also ensuring the application can be install with dependencies on
    other systems.

--- spec/helpers/nockConfig.js
+++
@@ -1,8 +0,0 @@
-const nock = require('nock');
-
-// - [node-nock/nock: HTTP mocking and expectations library]
-//   (https://github.com/node-nock/nock/)
-beforeAll(function () {
-  nock.disableNetConnect();
-  nock.enableNetConnect('localhost');
-});
